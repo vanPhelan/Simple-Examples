6705ca93f562c9ed9d4c90f0294e8482ca25f4ca
Van Phelan <lenamasdarkwood@netscape.net>
2020-06-10 16:21:46 -0500

    Initial commit

---
+++ Assets/Scripts/ColorPatternBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPatternBehavior : MonoBehaviour
+{
+    //Color to use for the shader
+    public Color color;
+
+    //Texture to use for the shader
+    public Texture2D pattern;
+
+    //Refernce to the Renderer Material
+    private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the material from the Renderer
+        material = GetComponent<Renderer>().material;
+
+        //Set the texture
+        material.SetTexture("Pattern", pattern);
+
+        //Set the color
+        material.SetColor("ColorBlend", color);
+    }
+}
---
+++ Assets/Scripts/PlaySoundBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundBehavior : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    //Play when the GameObject is clicked
+    public void OnMouseDown()
+    {
+        audioSource.Play();
+    }
+}
